abeaadbff90a89739fa1adde3e8e6237f9094737
mohuangrui <mohuangrui@gmail.com>
2015-02-22 20:51:01 -0500

    update

--- cfdcommons.c
+++ cfdcommons.c
@@ -206,6 +206,18 @@ int ComputeViscousFlux(const Field *field, Flux *flux, const Space *space, const
      * Auxiliary variables
      */
     Real divV = 0; /* divergence of velocity V */
+    Real du_dx = 0; /* partial u partial x */
+    Real du_dy = 0;
+    Real du_dz = 0;
+    Real dv_dx = 0; /* partial v partial x */
+    Real dv_dy = 0;
+    Real dv_dz = 0;
+    Real dw_dx = 0; /* partial w partial x */
+    Real dw_dy = 0;
+    Real dw_dz = 0;
+    Real dT_dx = 0; /* partial T partial x */
+    Real dT_dy = 0;
+    Real dT_dz = 0;
     const Real dx = MinPositive(space->dx, -1);
     const Real dy = MinPositive(space->dy, -1);
     const Real dz = MinPositive(space->dz, -1);
@@ -222,9 +234,9 @@ int ComputeViscousFlux(const Field *field, Flux *flux, const Space *space, const
     int idxN = 0; /* index at North */
     int idxF = 0; /* index at Front */
     int idxB = 0; /* index at Back */
-    for (k = 1; k < space->kMax - 1; ++k) {
-        for (j = 1; j < space->jMax - 1; ++j) {
-            for (i = 1; i < space->iMax - 1; ++i) {
+    for (k = 0; k < space->kMax; ++k) {
+        for (j = 0; j < space->jMax; ++j) {
+            for (i = 0; i < space->iMax; ++i) {
                 idx = (k * space->jMax + j) * space->iMax + i;
                 if (space->ghostFlag[idx] == -1) { /* if it's solid node */
                     continue;
@@ -237,29 +249,82 @@ int ComputeViscousFlux(const Field *field, Flux *flux, const Space *space, const
                 idxF = ((k - 1) * space->jMax + j) * space->iMax + i;
                 idxB = ((k + 1) * space->jMax + j) * space->iMax + i;
 
-                divV = (u[idxE] - u[idxW]) / (2 * dx) + 
-                    (v[idxN] - v[idxS]) / (2 * dy) + 
-                    (w[idxB] - w[idxF]) / (2 * dz);
+                if (k == 0) {
+                    du_dz = (u[idxB] - u[idx]) / dz;
+                    dv_dz = (v[idxB] - v[idx]) / dz;
+                    dw_dz = (w[idxB] - w[idx]) / dz;
+                    dT_dz = (T[idxB] - T[idx]) / dz;
+                } else {
+                    if (k == space->kMax -1) {
+                        du_dz = (u[idx] - u[idxF]) / dz;
+                        dv_dz = (v[idx] - v[idxF]) / dz;
+                        dw_dz = (w[idx] - w[idxF]) / dz;
+                        dT_dz = (T[idx] - T[idxF]) / dz;
+                    } else {
+                        du_dz = (u[idxB] - u[idxF]) / (2 * dz);
+                        dv_dz = (v[idxB] - v[idxF]) / (2 * dz);
+                        dw_dz = (w[idxB] - w[idxF]) / (2 * dz);
+                        dT_dz = (T[idxB] - T[idxF]) / (2 * dz);
+                    }
+                }
+                if (j == 0) {
+                    du_dy = (u[idxN] - u[idx]) / dy;
+                    dv_dy = (v[idxN] - v[idx]) / dy;
+                    dw_dy = (w[idxN] - w[idx]) / dy;
+                    dT_dy = (T[idxN] - T[idx]) / dy;
+                } else {
+                    if (j == space->jMax -1) {
+                        du_dy = (u[idx] - u[idxS]) / dy;
+                        dv_dy = (v[idx] - v[idxS]) / dy;
+                        dw_dy = (w[idx] - w[idxS]) / dy;
+                        dT_dy = (T[idx] - T[idxS]) / dy;
+                    } else {
+                        du_dy = (u[idxN] - u[idxS]) / (2 * dy);
+                        dv_dy = (v[idxN] - v[idxS]) / (2 * dy);
+                        dw_dy = (w[idxN] - w[idxS]) / (2 * dy);
+                        dT_dy = (T[idxN] - T[idxS]) / (2 * dy);
+                    }
+                }
+                if (i == 0) {
+                    du_dx = (u[idxE] - u[idx]) / dx;
+                    dv_dx = (v[idxE] - v[idx]) / dx;
+                    dw_dx = (w[idxE] - w[idx]) / dx;
+                    dT_dx = (T[idxE] - T[idx]) / dx;
+                } else {
+                    if (i == space->iMax -1) {
+                        du_dx = (u[idx] - u[idxW]) / dx;
+                        dv_dx = (v[idx] - v[idxW]) / dx;
+                        dw_dx = (w[idx] - w[idxW]) / dx;
+                        dT_dx = (T[idx] - T[idxW]) / dx;
+                    } else {
+                        du_dx = (u[idxE] - u[idxW]) / (2 * dx);
+                        dv_dx = (v[idxE] - v[idxW]) / (2 * dx);
+                        dw_dx = (w[idxE] - w[idxW]) / (2 * dx);
+                        dT_dx = (T[idxE] - T[idxW]) / (2 * dx);
+                    }
+                }
+
+                divV = du_dx + dv_dy + dw_dz;
 
                 Gx[0][idx] = 0;
-                Gx[1][idx] = flow->mu * ((u[idxE] - u[idxW]) / dx - (2/3) * divV);
-                Gx[2][idx] = flow->mu * ((u[idxN] - u[idxS]) / (2 * dy) + (v[idxE] - v[idxW]) / (2 * dx));
-                Gx[3][idx] = flow->mu * ((u[idxB] - u[idxF]) / (2 * dz) + (w[idxE] - w[idxW]) / (2 * dx));
-                Gx[4][idx] = flow->heatK * (T[idxE] - T[idxW]) / (2 * dx) + 
+                Gx[1][idx] = flow->mu * (2 * du_dx - (2/3) * divV);
+                Gx[2][idx] = flow->mu * (du_dy + dv_dx);
+                Gx[3][idx] = flow->mu * (du_dz + dw_dx);
+                Gx[4][idx] = flow->heatK * dT_dx + 
                     u[idx] * Gx[1][idx] + v[idx] * Gx[2][idx] + w[idx] * Gx[3][idx];
 
                 Gy[0][idx] = 0;
                 Gy[1][idx] = Gx[2][idx];
-                Gy[2][idx] = flow->mu * ((v[idxN] - v[idxS]) / dy - (2/3) * divV);
-                Gy[3][idx] = flow->mu * ((v[idxB] - v[idxF]) / (2 * dz) + (w[idxN] - w[idxS]) / (2 * dy));
-                Gy[4][idx] = flow->heatK * (T[idxN] - T[idxS]) / (2 * dy) + 
+                Gy[2][idx] = flow->mu * (2 * dv_dy - (2/3) * divV);
+                Gy[3][idx] = flow->mu * (dv_dz + dw_dy);
+                Gy[4][idx] = flow->heatK * dT_dy + 
                     u[idx] * Gy[1][idx] + v[idx] * Gy[2][idx] + w[idx] * Gy[3][idx];
 
                 Gz[0][idx] = 0;
                 Gz[1][idx] = Gx[3][idx];
                 Gz[2][idx] = Gy[3][idx];
-                Gz[3][idx] = flow->mu * ((w[idxB] - w[idxF]) / dz - (2/3) * divV);
-                Gz[4][idx] = flow->heatK * (T[idxB] - T[idxF]) / (2 * dz) + 
+                Gz[3][idx] = flow->mu * (2 * dw_dz - (2/3) * divV);
+                Gz[4][idx] = flow->heatK * dT_dz + 
                     u[idx] * Gz[1][idx] + v[idx] * Gz[2][idx] + w[idx] * Gz[3][idx];
             }
         }
